66a3248219ee41b0c27e8305e606d5657ee3e51b
Andrew Cole <andrew.cole@illallangi.com>
2014-01-20 21:30:06 +1100

    Removed Octokit.net

--- src/Illallangi.GitHub.PowerShell/GitHubModule.cs
+++ src/Illallangi.GitHub.PowerShell/GitHubModule.cs
@@ -6,7 +6,6 @@ using Illallangi.GitHub.Config;
 using Illallangi.GitHub.PowerShell.Config;
 
 using Ninject.Modules;
-using Octokit;
 
 namespace Illallangi.GitHub.PowerShell
 {
@@ -14,16 +13,12 @@ namespace Illallangi.GitHub.PowerShell
     {
         public override void Load()
         {
-
-            this.Bind<IGitHubClient>().ToMethod(cx => new GitHubClient(new ProductHeaderValue("Blah")));
-
             this.Bind<IGitHubConfig>()
                 .ToMethod(
                     cx =>
                     (GitHubConfig)
                     ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location)
                         .GetSection("GitHubConfig"));
-
         }
     }
 }
