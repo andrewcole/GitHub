0f141f84c2a85bc607dc28c3dabc411feb21dcdc
Andrew Cole <andrew.cole@illallangi.com>
2014-01-20 21:40:18 +1100

    Added Log4Net

--- src/Illallangi.GitHub.PowerShell/GitHubModule.cs
+++ src/Illallangi.GitHub.PowerShell/GitHubModule.cs
@@ -1,8 +1,14 @@
-﻿using System.Configuration;
+﻿using System.Collections.Specialized;
+using System.Configuration;
 using System.Reflection;
+
+using Common.Logging;
+using Common.Logging.Log4Net;
 
 using Illallangi.GitHub.Config;
 using Illallangi.GitHub.PowerShell.Config;
+
+using log4net.Config;
 
 using Ninject.Modules;
 
@@ -12,12 +18,24 @@ namespace Illallangi.GitHub.PowerShell
     {
         public override void Load()
         {
+            LogManager.Adapter = new Log4NetLoggerFactoryAdapter(new NameValueCollection { { "configType", "EXTERNAL" } });
+
+            XmlConfigurator.Configure(
+                Assembly
+                    .GetExecutingAssembly()
+                    .GetManifestResourceStream(
+                        string.Format(
+                            "{0}.Log4Net.config", 
+                            Assembly.GetExecutingAssembly().GetName().Name)));
+
             this.Bind<IGitHubConfig>()
                 .ToMethod(
                     cx =>
                     (GitHubConfig)
                     ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location)
                         .GetSection("GitHubConfig"));
+
+            this.Bind<ILog>().ToMethod(cx => LogManager.GetLogger(cx.Request.Target.Type));
         }
     }
 }
