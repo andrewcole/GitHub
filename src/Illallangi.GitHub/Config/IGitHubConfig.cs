4897b317c44244eaf749a3478aca75214f74a048
Andrew Cole <andrew.cole@illallangi.com>
2014-01-20 19:31:11 +1100

    Added IGitHubConfig

---
+++ src/Illallangi.GitHub/Config/IGitHubConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Illallangi.GitHub.Config
+{
+    public interface IGitHubConfig
+    {
+        string BaseUrl { get; }
+
+        IEnumerable<KeyValuePair<string, string>> GetDefaultParameters();
+    }
+}
